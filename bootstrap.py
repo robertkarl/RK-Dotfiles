9f70418daaaa40f5ad24403cb038d5f6277573a9
Robert Karl <robertkarl@users.noreply.github.com>
2018-02-17 14:19:26 -0800

    print nicer text, support other files besides vimrc

--- bootstrap.py
+++ bootstrap.py
@@ -6,30 +6,36 @@ import shutil
 import time
 
 VIMRC = ".vimrc"
-USER_VIMRC = os.path.expanduser(os.path.join("~", VIMRC))
+
+def user_dotfile_path(dotfile):
+    return os.path.expanduser(os.path.join("~", dotfile))
+
 
 def get_date_str():
     return time.strftime("%m%d-%I.%M%p")
 
-def bootstrap_vim():
-    copy_ops = []
-    if os.path.exists(USER_VIMRC):
-        copy_ops.append((USER_VIMRC, VIMRC + ".backup." + get_date_str()))
-    copy_ops.append((VIMRC, USER_VIMRC))
+
+def bootstrap(dotfile):
+    userpath = user_dotfile_path(dotfile)
+    copy_ops = [(dotfile, userpath)]
+    if os.path.exists(userpath):
+        copy_ops.insert(0, (userpath, dotfile + ".backup." + get_date_str()))
     return copy_ops
+
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--do_it", action='store_true')
     args = parser.parse_args()
-    copy_ops = bootstrap_vim()
+    copy_ops = bootstrap(VIMRC)
     if not args.do_it:
-        print(copy_ops)
+        for src, dst in copy_ops:
+            print("would move {} --> {}".format(src, dst))
     else:
-        print("performing the following copies")
-        print(copy_ops)
         for source, dest in copy_ops:
+            print("{} --> {}".format(source, dest))
             shutil.copy(source, dest)
+
 
 if __name__ == "__main__":
     main()
